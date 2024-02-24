31c3bcb27e93f2fd54367ad99888d6a7c74fa8f8
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-24 04:58:27 +0100

    styled the project with tailwind css

--- src/component/hooks/Card.jsx
+++ src/component/hooks/Card.jsx
@@ -19,25 +19,30 @@ const Card = () => {
 
   // our app logic
   return (
-    <div>
+    <div className="flex flex-col text-center items-center justify-center p-8 bg-zinc-400 gap-4 ">
       <button
         onClick={() => {
           setTourData(data)
         }}
+        className="w-[140px] h-[40px] bg-slate-600 text-white"
       >
         rest
       </button>
-      <div>
+      <div className="flex flex-col text-center items-center justify-center gap-4">
         {tourData.map((info) => {
           return (
-            <div key={info.id}>
+            <div
+              key={info.id}
+              className="w-[340px] h-[700px] flex flex-col text-start justify-center items-center bg-zinc-300 overflow-auto p-2 gap-4"
+            >
               <img src={info.image} alt="" />
               <h3>{info.name}</h3>
-              <p>{info.info}</p>
+              <p className=" text-sm">{info.info}</p>
               <button
                 onClick={() => {
                   RemoveTour(info.id, tourData, setTourData)
                 }}
+                className="w-[140px] h-[40px] bg-slate-600 text-white"
               >
                 remove
               </button>
